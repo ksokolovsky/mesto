4d2298750c6896ecde436b6c5573b079cf742809
ksokolovsky <k.sokolovskiy@yandex.ru>
2023-03-14 16:31:14 +0100

    Исправил почти все, что прислал ревьюер

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,43 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButton = popupElement.querySelector('.popup__close');
 const profileEditButton = document.querySelector('.profile__edit-button');
-
-const profileInputProffesion = popupElement.querySelector('.popup__input-profession');
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
+const profileInputProfession = popupElement.querySelector('#popup__input-profession');
 const profileSaveButton = document.querySelector('.popup__save-button');
 
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let profileInputName = popupElement.querySelector('#popup__input-name');
+let profileInputProffesion = popupElement.querySelector('#popup__input-profession');
+
+
 const openPopup = function() {
+    profileInputName.innerHTML = `${profileName.value}`;
+    profileInputProfession.innerHTML = `${profileProfession.value}`;
     popupElement.classList.add('popup_opened');
 }
 
-const closePopup = function(event) {
+const closePopup = function() {
     popupElement.classList.remove('popup_opened');
     
 }
 
-const ClosePopupByOverlayClick = function (event) {
+const closePopupByOverlayClick = function (event) {
+    event.preventDefault();
     if(event.target === event.currentTarget) {
         closePopup();
     }
-    event.preventDefault();
+    
 } 
 
-const editProfile = function() {
-    let profileInputName = popupElement.querySelector('.popup__input-name');
-    let profileInputProffesion = popupElement.querySelector('.popup__input-profession');
+const editProfile = function(event) {
+    event.preventDefault();
     profileName.innerHTML = `${profileInputName.value}`;
-    profileProfession.innerHTML = `${profileInputProffesion.value}`;
+    profileProfession.innerHTML = `${profileInputProfession.value}`;
     closePopup();
 }
 
 
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-popupElement.addEventListener('click', ClosePopupByOverlayClick);
+popupElement.addEventListener('click', closePopupByOverlayClick);
 profileSaveButton.addEventListener('click', editProfile);
