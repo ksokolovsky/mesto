d313dfa850f1df63046eec7aaa059101bf7d6c7b
ksokolovsky <k.sokolovskiy@yandex.ru>
2023-04-13 21:18:07 +0200

    Закончил работу. Жду правок от ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -18,37 +18,7 @@ const cardTemplate = document.getElementById('card-template');
 const cardGrid = document.querySelector('.elements');
 
 const zoomImagePopup = document.querySelector('.popup-zoom');
-const zoomImagePopupCloseButton = zoomImagePopup.querySelector('.popup-zoom__close-button')
-
-
-
-// const initialCards = [
-//     {
-//       name: 'Архыз',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//     },
-//     {
-//       name: 'Челябинская область',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//     },
-//     {
-//       name: 'Иваново',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//     },
-//     {
-//       name: 'Камчатка',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//     },
-//     {
-//       name: 'Холмогорский район',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//     },
-//     {
-//       name: 'Байкал',
-//       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//     }
-//   ];
-
+const zoomImagePopupCloseButton = zoomImagePopup.querySelector('.popup-zoom__close-button');
 
 
 // Добавление карточки. Забор из данных из инпутов
